feb257de0a78eec83237f47de3d958d8b1fb5614
Grant Bruer <grant.git.email@gmail.com>
2024-09-13 15:36:14 -0400

    Added StatisticsExt

--- examples/_utils/stats.jl
+++ examples/_utils/stats.jl
@@ -1,19 +1,3 @@
 function rmse(ensemble, y_true)
     sqrt(mean((ensemble .- y_true).^2))
 end
-
-function (@__MODULE__).mean(ensemble::Ensemble{K, V}) where {K, V}
-    m = Dict{K, V}()
-    for key in ensemble.state_keys
-        m[key] = mean(em[key] for em in ensemble.members)
-    end
-    return m
-end
-
-function (@__MODULE__).var(ensemble::Ensemble{K, V}) where {K, V}
-    m = Dict{K, V}()
-    for key in ensemble.state_keys
-        m[key] = var([em[key] for em in ensemble.members])
-    end
-    return m
-end
