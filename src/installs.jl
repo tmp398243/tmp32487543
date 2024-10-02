259db599579800faed8e53d5492b0807073a40fe
Grant Bruer <grant.git.email@gmail.com>
2024-10-02 11:52:27 -0400

    Removed references to InvertibleNetworks

--- src/installs.jl
+++ src/installs.jl
@@ -6,11 +6,7 @@ _dependencies[:Lorenz63] =
 _dependencies[:EnsembleKalmanFilters] =
     () -> Pkg.add(; url="https://github.com/tmp398243/tmp45742")
 _dependencies[:NormalizingFlowFilters] =
-    () -> begin
-        # Need the latest version of InvertibleNetworks for the odd input size fix.
-        Pkg.add(; url="https://github.com/tmp398243/tmp337502")
-        Pkg.add(; url="https://github.com/slimgroup/InvertibleNetworks.jl/")
-    end
+    () -> Pkg.add(; url="https://github.com/tmp398243/tmp337502")
 
 function install(pkg::Symbol)
     if !(pkg in keys(_dependencies))
